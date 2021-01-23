5df5564e4593a303f32a3993f480e44074242217
Saquib <saquibjawed0@gmail.com>
2021-01-24 01:46:54 +0530

    initial commit

--- regis/settings.py
+++ regis/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'cx4p&_xgdjxl5+gb!=fdk^$cpj@fyq-wky4hkhct&y3t6)7q*^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','regis-a.herokuapp.com']
 
 
 # Application definition
